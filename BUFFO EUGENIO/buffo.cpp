baee55deb242e6dba6cfd6c638ac46c871b7ff48
BuffoEu <cptcasa01@gmail.com>
2016-03-01 12:21:12 +0100

    Cartelle

---
+++ BUFFO EUGENIO/buffo.cpp
@@ -0,0 +1,11 @@
+/*-----------------------------------------
+BUFFO EUGENIO IVC INFORMATICA 01/03/2K16
+-----------------------------------------*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	cout<<"HELLO WORLD!";
+	return 0;
+}
